8b7c3f3d901821f00b4fb898c7f93470827db328
Alexandr Rozenberg <dgonial@gmail.com>
2023-12-27 23:18:05 +0300

    + task

---
+++ src/exercises/leedcode/task28/Solution.java
@@ -0,0 +1,22 @@
+package exercises.leedcode.task28;
+
+public class Solution {
+    public static int strStr(String haystack, String needle){
+        if (needle.isEmpty())
+            return 0;
+
+        if (needle.length() > haystack.length())
+            return -1;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++){
+            int j = 0;
+            while (j < needle.length() && haystack.charAt(i + j) == needle.charAt(j)){
+                j++;
+            }
+            if (j == needle.length()){
+                return 1;
+            }
+        }
+        return -1;
+    }
+}
