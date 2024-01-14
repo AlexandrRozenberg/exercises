2e997519d7c9dcf6d3428d686e233e8ca2e0d964
Alexandr Rozenberg <dgonial@gmail.com>
2024-01-14 23:10:50 +0300

    + task

---
+++ src/exercises/leedcode/task108/Solution.java
@@ -0,0 +1,31 @@
+package exercises.leedcode.task108;
+
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums){
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums, int start, int end){
+        if (start > end)
+            return null;
+
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, start, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, end);
+
+        return root;
+    }
+}
