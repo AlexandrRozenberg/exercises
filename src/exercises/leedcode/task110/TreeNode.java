ad8b12175030d155842489950a93753c8ca5987d
Alexandr Rozenberg <dgonial@gmail.com>
2024-01-16 20:40:15 +0300

    + task

---
+++ src/exercises/leedcode/task110/TreeNode.java
@@ -0,0 +1,12 @@
+package exercises.leedcode.task110;
+
+public class TreeNode {
+        int val;
+        exercises.leedcode.task110.TreeNode left;
+        exercises.leedcode.task110.TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+}
