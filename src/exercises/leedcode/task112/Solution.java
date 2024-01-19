10050707046940daad1b9d1b596295d8e4cb69eb
Alexandr Rozenberg <dgonial@gmail.com>
2024-01-19 23:32:55 +0300

    + task

---
+++ src/exercises/leedcode/task112/Solution.java
@@ -0,0 +1,35 @@
+package exercises.leedcode.task112;
+
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+
+        int targetSum = 22;
+
+        Solution solution = new Solution();
+        boolean result = solution.hasPathSum(root, targetSum);
+        System.out.println(result); // Output: true
+    }
+}
+
+
---
+++ src/exercises/leedcode/task112/TreeNode.java
@@ -0,0 +1,11 @@
+package exercises.leedcode.task112;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
