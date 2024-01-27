15006f47d59b29102ddbb2768429c72786ac18e8
Alexandr Rozenberg <dgonial@gmail.com>
2024-01-27 21:48:13 +0300

    + task

--- src/exercises/leedcode/task121/Solution.java
+++ src/exercises/leedcode/task121/Solution.java
@@ -1,4 +1,21 @@
 package exercises.leedcode.task121;
 
 public class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
+
 }
