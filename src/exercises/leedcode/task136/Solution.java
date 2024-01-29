2606ab89f97ca09554ff3512d47ef5b123ea00ad
Alexandr Rozenberg <dgonial@gmail.com>
2024-01-29 23:51:00 +0300

    + task

---
+++ src/exercises/leedcode/task136/Solution.java
@@ -0,0 +1,11 @@
+package exercises.leedcode.task136;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
