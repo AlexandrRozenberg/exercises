b7a01b4118ea0652bf0dbfc13cab2d77dc3f5116
Alexandr Rozenberg <dgonial@gmail.com>
2024-01-31 23:01:14 +0300

    + task

---
+++ src/exercises/leedcode/task144/Solution.java
@@ -0,0 +1,32 @@
+package exercises.leedcode.task144;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class  TreeNode{
+    int var;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int var) {
+        this.var = var;
+    }
+}
+
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        preorderTraversalHelper(root, result);
+        return result;
+    }
+
+    private void preorderTraversalHelper(TreeNode node, List<Integer> result){
+        if (node == null)
+            return;
+
+        result.add(node.var){
+            preorderTraversalHelper(node.left, result);
+            preorderTraversalHelper(node.right, result);
+        }
+    }
+}
