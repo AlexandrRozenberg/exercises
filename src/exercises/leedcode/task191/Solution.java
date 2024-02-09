4f8e930d500859a47920da7abefa632754d45cf7
Alexandr Rozenberg <dgonial@gmail.com>
2024-02-09 23:46:47 +0300

    + task

---
+++ src/exercises/leedcode/task191/Solution.java
@@ -0,0 +1,12 @@
+package exercises.leedcode.task191;
+
+public class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>>= 1;
+        }
+        return count;
+    }
+}
