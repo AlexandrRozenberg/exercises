b1ab0753d29791ecb287d25481345d6a9cde8001
Alexandr Rozenberg <dgonial@gmail.com>
2024-02-21 23:52:58 +0300

    + task

--- src/exercises/leedcode/task202/Solution.java
+++ src/exercises/leedcode/task202/Solution.java
@@ -1,24 +1,27 @@
 package exercises.leedcode.task202;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
-    public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
+        public boolean isHappy(int n) {
+            Set<Integer> seen = new HashSet<>();
 
-        while (n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = getNext(n);
+            while (n != 1 && !seen.contains(n)) {
+                seen.add(n);
+                n = getNext(n);
+            }
+
+            return n == 1;
         }
 
-        return n == 1;
-    }
-
-    private int getNext(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
+        private int getNext(int n) {
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            return sum;
         }
-        return sum;
     }
-}
