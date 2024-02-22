28bb2e8763e0518c0109ad06724f19a26d115db5
Alexandr Rozenberg <dgonial@gmail.com>
2024-02-22 23:50:35 +0300

    + task

---
+++ src/exercises/leedcode/task203/Solution.java
@@ -0,0 +1,28 @@
+package exercises.leedcode.task203;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+
+        while (curr.next != null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
