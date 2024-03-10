35bae1982a042b540dec5c5752be104ed60d3220
Alexandr Rozenberg <dgonial@gmail.com>
2024-03-10 23:51:47 +0300

    + task

---
+++ src/exercises/leedcode/task1672/Solution.java
@@ -0,0 +1,13 @@
+package exercises.leedcode.task1672;
+
+public class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int largest = accounts[0][0];
+        for(int[] customer : accounts){
+            int currentSum = 0;
+            for(int bank : customer) currentSum += bank;
+            largest = Math.max(largest, currentSum);
+        }
+        return largest;
+    }
+}
