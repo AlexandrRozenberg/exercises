bd72db8c393ecbdde9d214c8448dbbef569f1159
Alexandr Rozenberg <dgonial@gmail.com>
2024-03-20 23:32:32 +0300

    + task

---
+++ src/exercises/leedcode/task1365/Solution.java
@@ -0,0 +1,26 @@
+package exercises.leedcode.task1365;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] output = new int[nums.length];
+        ArrayList<Integer> list = (ArrayList<Integer>) Arrays.stream(nums).boxed().collect(Collectors.toList());
+        Collections.sort(list);
+
+        for (int i = 0; i <= nums.length - 1; i++) {
+            int n = 0;
+            int index = list.indexOf(nums[i]);
+
+            for (int j = index - 1; j >= 0; j--) {
+                if (nums[i] > list.get(j))
+                    n++;
+            }
+            output[i] = n;
+        }
+        return output;
+    }
+}
