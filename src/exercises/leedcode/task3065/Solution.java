9c858aed1fa0e439db73955d5f1628d435db0261
Alexandr Rozenberg <dgonial@gmail.com>
2024-04-05 22:05:23 +0300

    + task

---
+++ src/exercises/leedcode/task3065/Solution.java
@@ -0,0 +1,12 @@
+package exercises.leedcode.task3065;
+
+public class Solution {
+    public int minOperations(int[] nums, int k) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < k)
+                count++;
+        }
+        return count;
+    }
+}
